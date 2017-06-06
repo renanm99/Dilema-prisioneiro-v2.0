c6dbf19d22594d976238fee5d24f9892ac0c332d
Renan Machado <renanoliveira2199@gmail.com>
2017-06-06 15:57:24 -0300

    metodo de mensagens atualizado

--- src/main_package/jogo_dilema.java
+++ src/main_package/jogo_dilema.java
@@ -21,7 +21,7 @@ public class jogo_dilema {
         int res;//Variavel de resposta usuario
         int comparsa;//Variavel de resposta da maquina
         boolean jogo = true;//variavel que controla o loop do jogo inteiro
-        String estrategia;//variavel para decidir se troca de estrategia
+        String estrategia = "";//variavel para decidir se troca de estrategia
         boolean erro = false;//variavel que controla o repetiçao, caso haja algum erro
         
                 System.out.println(" ");
@@ -44,7 +44,6 @@ public class jogo_dilema {
                         + "\t 3 = PROVADOR INGÊNUO,"
                         + "\t 4 = RETALIADOR PERMANENTE,"
                         + "\t 5 = SAIR E FECHAR O JOGO");
-                        //
 
                         a = ler.nextInt();//ler estrategia do jogador
 
@@ -71,36 +70,21 @@ public class jogo_dilema {
                                 comparsa = (int) (Math.random()*2);
 
                                 if(res == 0 || res == 1){//teste para ver se digitou corretamente
-
-                                if(res == 0 && comparsa == 0){
-                                    cc++;//cooperar x cooperar
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("2 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 1){
-                                    tt++;// trair x trair
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("5 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 0){
-                                    tc++;// trair x cooperar
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("COMPARSA PEGOU 10 ANOS");
-                                    System.out.println("VOCÊ ESTÁ LIVRE");
-
-                                }else if(res == 0 && comparsa == 1){
-                                    tc++;
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("VOCÊ PEGOU 10 ANOS");
-                                    System.out.println("COMPARSA ESTÁ LIVRE");
-
-                                }
+                                    
+                                    int retorno = mensagens(res,comparsa,cc,tt,tc);
+                                    switch(retorno){
+                                        case 1: 
+                                            cc++;
+                                            break;
+                                        case 2:
+                                            tt++;
+                                            break;
+                                        default:
+                                            tc++;
+                                    }
+                                    
                                 System.out.println("------------------------------");
-
+                                
                                 do{
                                     System.out.println("\nDeseja continuar com a estratégia? (s/n)");
                                     estrategia = ler.next();
@@ -148,33 +132,18 @@ public class jogo_dilema {
 
                                 if(res == 0 || res == 1){//teste para ver se digitou corretamente
 
-                                if(res == 0 && comparsa == 0){
-                                    cc++;//cooperar x cooperar
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("2 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 1){
-                                    tt++;// trair x trair
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("5 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 0){
-                                    tc++;// trair x cooperar
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("COMPARSA PEGOU 10 ANOS");
-                                    System.out.println("VOCÊ ESTÁ LIVRE");
-
-                                }else if(res == 0 && comparsa == 1){
-                                    tc++;
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("VOCÊ PEGOU 10 ANOS");
-                                    System.out.println("COMPARSA ESTÁ LIVRE");
-
-                                }
+                                int retorno = mensagens(res,comparsa,cc,tt,tc);
+                                    switch(retorno){
+                                        case 1: 
+                                            cc++;
+                                            break;
+                                        case 2:
+                                            tt++;
+                                            break;
+                                        default:
+                                            tc++;
+                                    }
+                                    
                                 System.out.println("------------------------------");
 
                                 comparsa = res;
@@ -240,32 +209,16 @@ public class jogo_dilema {
                                         vez = 0;//senão vez continua 0 ou é volta para 0
                                     }
 
-                                    if(res == 0 && comparsa == 0){
-                                        cc++;
-                                        System.out.println("VOCÊ COOPEROU");
-                                        System.out.println("COMPARSA COOPEROU");
-                                        System.out.println("2 ANOS CADA");
-
-                                    }else if(res == 1 && comparsa == 1){
-                                        tt++;
-                                        System.out.println("VOCÊ TRAIU");
-                                        System.out.println("COMPARSA TRAIU");
-                                        System.out.println("5 ANOS CADA");
-
-                                    }else if(res == 1 && comparsa == 0){
-                                        tc++;
-                                        System.out.println("VOCÊ TRAIU");
-                                        System.out.println("COMPARSA COOPEROU");
-                                        System.out.println("COMPARSA PEGOU 10 ANOS");
-                                        System.out.println("VOCÊ ESTÁ LIVRE");
-
-                                    }else if(res == 0 && comparsa == 1){
-                                        tc++;
-                                        System.out.println("VOCÊ COOPEROU");
-                                        System.out.println("COMPARSA TRAIU");
-                                        System.out.println("VOCÊ PEGOU 10 ANOS");
-                                        System.out.println("COMPARSA ESTÁ LIVRE");
-
+                                    int retorno = mensagens(res,comparsa,cc,tt,tc);
+                                    switch(retorno){
+                                        case 1: 
+                                            cc++;
+                                            break;
+                                        case 2:
+                                            tt++;
+                                            break;
+                                        default:
+                                            tc++;
                                     }
 
                              System.out.println("------------------------------");
@@ -321,33 +274,18 @@ public class jogo_dilema {
 
                                 if(res == 0 || res == 1){//teste para ver se digitou corretamente
 
-                                if(res == 0 && comparsa == 0){
-                                    cc++;//cooperar x cooperar
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("2 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 1){
-                                    tt++;// trair x trair
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("5 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 0){
-                                    tc++;// trair x cooperar
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("COMPARSA PEGOU 10 ANOS");
-                                    System.out.println("VOCÊ ESTÁ LIVRE");
-
-                                }else if(res == 0 && comparsa == 1){
-                                    tc++;
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("VOCÊ PEGOU 10 ANOS");
-                                    System.out.println("COMPARSA ESTÁ LIVRE");
-
-                                }
+                                int retorno = mensagens(res,comparsa,cc,tt,tc);
+                                    switch(retorno){
+                                        case 1: 
+                                            cc++;
+                                            break;
+                                        case 2:
+                                            tt++;
+                                            break;
+                                        default:
+                                            tc++;
+                                    }
+                                    
                                 System.out.println("------------------------------");
 
                                 do{
@@ -394,33 +332,18 @@ public class jogo_dilema {
 
                                 if(res == 0 || res == 1){//teste para ver se digitou corretamente
 
-                                if(res == 0 && comparsa == 0){
-                                    cc++;//cooperar x cooperar
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("2 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 1){
-                                    tt++;// trair x trair
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("5 ANOS CADA");
-
-                                }else if(res == 1 && comparsa == 0){
-                                    tc++;// trair x cooperar
-                                    System.out.println("VOCÊ TRAIU");
-                                    System.out.println("COMPARSA COOPEROU");
-                                    System.out.println("COMPARSA PEGOU 10 ANOS");
-                                    System.out.println("VOCÊ ESTÁ LIVRE");
-
-                                }else if(res == 0 && comparsa == 1){
-                                    tc++;
-                                    System.out.println("VOCÊ COOPEROU");
-                                    System.out.println("COMPARSA TRAIU");
-                                    System.out.println("VOCÊ PEGOU 10 ANOS");
-                                    System.out.println("COMPARSA ESTÁ LIVRE");
-
-                                }
+                                int retorno = mensagens(res,comparsa,cc,tt,tc);
+                                    switch(retorno){
+                                        case 1: 
+                                            cc++;
+                                            break;
+                                        case 2:
+                                            tt++;
+                                            break;
+                                        default:
+                                            tc++;
+                                    }
+                                    
                                 System.out.println("------------------------------");
 
                                 if (res == 1) {
@@ -506,6 +429,41 @@ public class jogo_dilema {
                 teste e comparaçao de porcentagem
                 */
 }
+    
+    
+    public static int mensagens(int res, int comparsa, int cc, int tt, int tc){
+        if(res == 0 && comparsa == 0){
+            //cc++;//cooperar x cooperar
+            System.out.println("VOCÊ COOPEROU");
+            System.out.println("COMPARSA COOPEROU");
+            System.out.println("2 ANOS CADA");
+            return 1;
+
+        }else if(res == 1 && comparsa == 1){
+            //tt++;// trair x trair
+            System.out.println("VOCÊ TRAIU");
+            System.out.println("COMPARSA TRAIU");
+            System.out.println("5 ANOS CADA");
+            return 2;
+
+        }else if(res == 1 && comparsa == 0){
+            //tc++;// trair x cooperar
+            System.out.println("VOCÊ TRAIU");
+            System.out.println("COMPARSA COOPEROU");
+            System.out.println("COMPARSA PEGOU 10 ANOS");
+            System.out.println("VOCÊ ESTÁ LIVRE");
+            return 3;
+
+        }else{
+            //tc++;
+            System.out.println("VOCÊ COOPEROU");
+            System.out.println("COMPARSA TRAIU");
+            System.out.println("VOCÊ PEGOU 10 ANOS");
+            System.out.println("COMPARSA ESTÁ LIVRE");
+            return 3;
+        }
+    }
+    
 }
         
 //fim de classe
